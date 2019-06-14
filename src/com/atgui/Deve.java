53cbd943283fa2345f0e26dbca50cb230ef8fb56
baozhu <775301087@qq.com>
2019-06-14 20:55:26 +0800

    创建新增功能

---
+++ src/com/atgui/Deve.java
@@ -0,0 +1,13 @@
+package com.atgui;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+public class Deve {
+	public static void main(String[] args) {
+		System.out.println("创建文件");
+		System.out.println("我是他的主线");
+		System.out.println("我也改了");
+		System.out.println("我修改了一些");
+		System.out.println("我是一个新增的功能");
+	}
+}
